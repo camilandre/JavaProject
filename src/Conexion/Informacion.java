4cf023fb157674186817f6416c2a6762f8d9c8d5
Camila Andrea <75325895+camilandre@users.noreply.github.com>
2026-03-25 15:19:06 -0500

    Update Informacion.java

--- src/Conexion/Informacion.java
+++ src/Conexion/Informacion.java
@@ -4,7 +4,7 @@ public interface Informacion {
 
 		String bd  ="bd_clinicadental"; 
 		String usuario  ="camila"; 
-		String clave = "Mamitateamo28"; 
+		String clave = "password";
 		String url = "jdbc:mariadb://localhost/" + bd; 
 		String driver= "org.mariadb.jdbc.Driver"; 
 
